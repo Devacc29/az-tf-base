6361fef223e920bc92824aba157a56c096d5d2a2
Deva <dvrharsha@gmail.com>
2024-11-11 16:26:03 -0500

    check m

--- resourcegroup/resourcegroup.tf
+++ resourcegroup/resourcegroup.tf
@@ -1,3 +1,12 @@
+variable "resource_groups" {
+  type = map(object({
+    name     = string
+    location = string
+    tags     = map(string)
+  }))
+}
+
 module "base_rg" {
-    source = "value"
+  source          = "git::https://github.com/Devacc29/terragrunt-module.git//modules/resourcegroup"
+  resource_groups = var.resource_groups
 }
---
+++ resourcegroup/resourcegroup.tfvars
@@ -0,0 +1,8 @@
+rg1 = {
+  name     = "dev-rg-03-pipe"
+  location = "eastus2"
+  tags = {
+    env        = "dev"
+    created_by = "1234@gmail.com"
+  }
+}
