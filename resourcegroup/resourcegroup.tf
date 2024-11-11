cec1534998123a9f86ae62cd46ac6b2c10cd74e9
Deva <dvrharsha@gmail.com>
2024-11-11 16:32:53 -0500

    check

--- resourcegroup/resourcegroup.tf
+++ resourcegroup/resourcegroup.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  subscription_id                 = "c92973e5-7f67-4bc4-af10-1ed1c2f696c1"
+  features {}
+}
+
 variable "resource_groups" {
   type = map(object({
     name     = string
